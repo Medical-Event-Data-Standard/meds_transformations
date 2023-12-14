5e7520ee1602f6b138332e08fa60962edd96b881
Matthew McDermott <mattmcdermott8@gmail.com>
2023-12-14 10:23:40 -0600

    Added doctest to normalize.

--- src/MEDS_transformations/normalize.py
+++ src/MEDS_transformations/normalize.py
@@ -21,9 +21,35 @@ class NormalizeFntr(MEDSTransformationFntr):
         self.norm_params = norm_params
 
     def normalize_measurement(self, meas: dict[str, Any]) -> dict[str, Any]:
-        """Normalize a single measurement."""
+        """Normalize a single measurement to have zero mean and unit variance.
+
+        Args:
+            meas: A measurement dictionary with keys code and (optionally) numeric_value.
+
+        Returns:
+            The measurement dictionary with numeric_value normalized, if the code is in the norm_params and
+            numeric_value is not None.
+
+        Examples:
+            >>> norm_params = {"HR": (70, 10)}
+            >>> fntr = NormalizeFntr(norm_params)
+            >>> fntr.normalize_measurement({"code": "HR", "numeric_value": 80})
+            {'code': 'HR', 'numeric_value': 1.0}
+            >>> fntr.normalize_measurement({"code": "HR", "numeric_value": 70})
+            {'code': 'HR', 'numeric_value': 0.0}
+            >>> fntr.normalize_measurement({"code": "HR", "numeric_value": 60})
+            {'code': 'HR', 'numeric_value': -1.0}
+            >>> fntr.normalize_measurement({"code": "HR", "numeric_value": None})
+            {'code': 'HR', 'numeric_value': None}
+            >>> fntr.normalize_measurement({"code": "HR"})
+            {'code': 'HR'}
+            >>> fntr.normalize_measurement({"code": "HR", "numeric_value": np.nan})
+            {'code': 'HR', 'numeric_value': nan}
+            >>> fntr.normalize_measurement({"code": "BP", "numeric_value": 80})
+            {'code': 'BP', 'numeric_value': 80}
+        """
         code = meas["code"]
-        value = meas["numeric_value"]
+        value = meas.get("numeric_value", None)
         if code not in self.norm_params or value is None or value is np.nan:
             return meas
         mean, std = self.norm_params[code]
